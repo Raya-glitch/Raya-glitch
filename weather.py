5ecbbe437a3f04c979456e5c22aef53ba6015e77
Raya-glitch <yaram.gamer@gmail.com>
2025-02-09 00:57:02 +0000

    Not done

--- weather.py
+++ weather.py
@@ -2,13 +2,19 @@ import pandas as pd
 import matplotlib.pyplot as plot
 
 data = pd.read_csv("weather.csv")
-data.plot(kind="bar", y = "Location", x = "Weather Type")
+
+data.plot(subplots=True, x = "Season")
+
 plot.savefig("weather.png")
 
+temperature = data['Temperature']
 
+fig, ax = plot.subplots(7, 1, figsize=(6, 10))
 
+ax[0].plot(data, temperature, color='tab:red')
 
-
+df = pd.read_csv('weather.csv')
+print(df) 
 
 
 
