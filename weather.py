09bf5975b31e8502bc5f2c0ca0eac8f7815a1cea
Raya-glitch <yaram.gamer@gmail.com>
2025-02-10 19:42:32 +0000

    Not done

--- weather.py
+++ weather.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plot
 
 data = pd.read_csv("weather.csv")
 
-data.plot(subplots=True, x = "Season")
+data.plot(type=True, x = "Season")
+fig, ax = plot.subplots(7, 1, figsize=(5, 25))
 
+
+ax[0].barh(data["Temperature"], data["Weather Type"] )
+ax[0].set_xlabel("Temparature")
+ax[0].set_ylabel("Season")
+ax[0].set_title("Correlation of Temparature to Season")
 plot.savefig("weather.png")
-
-temperature = data['Temperature']
-
-fig, ax = plot.subplots(7, 1, figsize=(6, 10))
-
-ax[0].plot(data, temperature, color='tab:red')
 
 df = pd.read_csv('weather.csv')
 print(df) 
